637993b08f1a502f849bc846c1a3ff302990b57f
DO VAN HAI <haidv2801@gmail.com>
2022-07-27 23:44:29 +0700

    add

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var multiSelect = {
 
     (me.el = document.querySelector(el)),
       (me.el_input = me.el.querySelector("input[type='text']")),
-      (me.el_items = me.el.querySelectorAll('.multi-select__items'));
+      (me.el_items = me.el.querySelector('.multi-select__items'));
   },
 
   initEvents: function () {
@@ -19,13 +19,46 @@ var multiSelect = {
 
     if (me.el_input) {
       me.el_input.addEventListener('keydown', me.handleChangeInput);
+      me.el_input.addEventListener('focus', (e) =>
+        me.handleFocusInput.bind(me)(e)
+      );
+      me.el_input.addEventListener('blur', (e) =>
+        me.handleBlurInput.bind(me)(e)
+      );
     }
 
     if (me.el_items) {
-      me.el_items.forEach((item) => {
-        item.addEventListener('change', me.handleSelectItem.bind(me));
-      });
+      me.el_items.addEventListener('change', me.handleSelectItem.bind(me));
     }
+
+    window.onclick = (e) => me.handleClickOutSide(e, me);
+  },
+
+  handleClickOutSide: (e, me) => {
+    if (me.el_items.contains(e.target) || me.el_input.contains(e.target))
+      return;
+    me.hideMenu();
+  },
+
+  handleFocusInput: function (e) {
+    let me = this;
+
+    me.el_items.classList.remove('hide');
+  },
+
+  handleBlurInput: function (e) {},
+
+  hideMenu: function (e) {
+    let me = this;
+
+    me.el_items.classList.remove('hide');
+    me.el_items.classList.add('hide');
+  },
+
+  showMenu: function (e) {
+    let me = this;
+
+    me.el_items.classList.remove('hide');
   },
 
   handleChangeInput: function (e) {
@@ -34,14 +67,9 @@ var multiSelect = {
   },
 
   handleSelectItem: function (e) {
-    let me = this,
-      value = e.target.value;
+    let me = this;
 
-    console.log(
-      me.getCheckedNodes
-        .bind(Array)()
-        .map((node) => node)
-    );
+    me.el_input.value = me.getCheckedValues().join(';');
   },
 
   handleSearch: function (text) {},
@@ -49,7 +77,14 @@ var multiSelect = {
   getCheckedNodes: function () {
     let me = this;
 
-    return me.el_items[0].querySelectorAll('li input[type="checkbox"]:checked');
+    return me.el_items.querySelectorAll('li input[type="checkbox"]:checked');
+  },
+
+  getCheckedValues: function () {
+    let me = this,
+      checkedNodes = Array.from(me.getCheckedNodes()).map((node) => node.value);
+
+    return checkedNodes;
   },
 };
 
